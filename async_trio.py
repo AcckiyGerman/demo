4fa016f6b948674951d227f657565206282865f0
Dmytro Gierman <dgierman@heliosventilatoren.de>
2019-12-17 11:55:07 +0100

    async with trio v2

--- async_trio.py
+++ async_trio.py
@@ -4,7 +4,7 @@ import typing
 import trio
 import asks
 
-session = asks.sessions.Session(connections=2)
+session = asks.sessions.Session(connections=5)
 
 
 @dataclass
@@ -21,25 +21,20 @@ class Tree:
 
 async def map_tree(func, node):
     async with trio.open_nursery() as nursery:
-        # get the value from API and set it asynchronously
         nursery.start_soon(func, node)
         for child in node.children:
             nursery.start_soon(map_tree, func, child)
 
 
 async def get_value_for_node(node):
-    res = await session.get(f"https://jsonplaceholder.typicode.com/todos/{node.id}")
-    data = res.json()
-    print(f'response {node.id}: {data}')
-    node.value = data['title']
+    r = await session.get(f"https://jsonplaceholder.typicode.com/todos/{node.id}")
+    print(f"id {node.id} {'ok' if r.status_code==200 else 'err'}")
+    if r.status_code == 200:
+        node.value = r.json()['title']
 
 
-async def main():
-    tree = Tree(1, [Tree(2, [Tree(4, []), Tree(5, [])]), Tree(3, [])])
-    print(tree)
-
+async def main(tree):
     await map_tree(get_value_for_node, tree)
-    print(tree)
+    print('\n', tree)
 
-
-trio.run(main)
+trio.run(main, Tree(0, [Tree(2, [Tree(4, []), Tree(5, [])]), Tree(3, [])]))
